7c9fa57c1ed71ba0003d7718c940b9b5830100ae
Ted Kaemming <ted@kaemming.com>
2015-02-18 14:15:43 -0800

    Add ManagedConnection.
    Summary:
    This allows threadsafe access to an underlying PostgreSQL connection,
    manages transactional state, ensures the connection isn't left idle in
    transaction, etc.
    
    Test Plan: Added 'em
    
    Reviewers: tail, danf, jeff
    
    Reviewed By: jeff
    
    Differential Revision: http://phabricator.local.disqus.net/D16107

--- src/main/python/pgshovel/utilities/postgresql.py
+++ src/main/python/pgshovel/utilities/postgresql.py
@@ -1,7 +1,10 @@
 import logging
 import sys
+import threading
 import uuid
 from contextlib import contextmanager
+
+import psycopg2
 
 from pgshovel.utilities.exceptions import chained
 
@@ -94,3 +97,70 @@ def managed(transactions):
     # transactions.
     for transaction in prepared:
         transaction.commit()
+
+
+class ManagedConnection(object):
+    STATUS_IN_TRANSACTION = frozenset((
+        psycopg2.extensions.TRANSACTION_STATUS_INERROR,
+        psycopg2.extensions.TRANSACTION_STATUS_INTRANS,
+    ))
+
+    def __init__(self, dsn):
+        self.dsn = dsn
+
+        self.__connection = None
+
+        # Barrier for when the `__connection` attribute is being modified
+        # set/unset is being mutated to avoid data races.
+        self.__meta_lock = threading.Lock()
+
+        # Barrier to prevent multiple threads from using the connection at
+        # once.
+        self.__lock = threading.Lock()
+
+    @property
+    def closed(self):
+        with self.__meta_lock:
+            if self.__connection is None:
+                return True
+            return self.__connection.closed
+
+    @property
+    def status(self):
+        with self.__meta_lock:
+            if self.__connection is None:
+                return None
+            return self.__connection.get_transaction_status()
+
+    @contextmanager
+    def __call__(self):
+        # TODO: Support the ability to be non-blocking.
+        with self.__lock:
+            with self.__meta_lock:
+                if not self.__connection or self.__connection.closed:
+                    self.__connection = psycopg2.connect(self.dsn)
+
+            try:
+                yield self.__connection
+            except Exception as error:
+                if isinstance(error, psycopg2.Error):
+                    try:
+                        self.__connection.close()
+                    except Exception as error:
+                        logger.info('Could not close connection during recovery: %s', error, exc_info=True)
+                    finally:
+                        with self.__meta_lock:
+                            self.__connection = None
+                else:
+                    if self.__connection.get_transaction_status() in self.STATUS_IN_TRANSACTION:
+                        logger.warning('Forcing connection rollback after exception thrown in connection block: %s', error, exc_info=True)
+                        self.__connection.rollback()
+                raise
+            else:
+                # Check to make sure the client didn't leave the connection in a dirty state.
+                if self.__connection.get_transaction_status() in self.STATUS_IN_TRANSACTION:
+                    # We need to roll back the transaction in case the
+                    # exception we raise below is caught and the manager is
+                    # used again.
+                    self.__connection.rollback()
+                    raise RuntimeError("Did not commit or rollback open transaction before returning connection. Were you born in a barn? (We rolled it back for you, anyway.)")
